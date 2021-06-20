e40dc33469d56566f4105588b9bba4f193b2b2a5
Nikhilesh <nikhilesh.debbarma062@gmail.com>
2021-06-20 22:14:35 +0530

    make it better

---
+++ collectfee/urls.py
@@ -0,0 +1,9 @@
+
+from django.urls import path
+from . views import *
+urlpatterns = [
+    path('test-payment/', test_payment),
+    path('save-stripe-info/', save_stripe_info),
+    path('pay/', start_payment, name="payment"),
+    path('payment/success/', handle_payment_success, name="payment_success"),
+]
---
+++ collectfee/views.py
@@ -0,0 +1,158 @@
+from django.shortcuts import render
+import stripe
+from rest_framework.response import Response
+# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework import status
+
+stripe.api_key = 'sk_test_51B8dmzKYbyr1xLhvvWluiihZYPwNoQuzQbUMEaywd91dQ0q1hFJ7SdYHc2uxEis3i8nIZPeutsBzjqqHyRNLTDR8009F1BbuPH'
+
+
+@api_view(['POST'])
+def test_payment(request):
+    test_payment_intent = stripe.PaymentIntent.create(
+    amount=1000, currency='pln', 
+    payment_method_types=['card'],
+    receipt_email='test@example.com')
+    return Response(status=status.HTTP_200_OK, data=test_payment_intent)
+@api_view(['POST'])
+def save_stripe_info(request):
+    data = request.data
+    email = data['email']
+    payment_method_id = data['payment_method_id']
+    extra_msg = '' # add new variable to response message
+
+    # checking if customer with provided email already exists
+    customer_data = stripe.Customer.list(email=email).data
+
+    # if the array is empty it means the email has not been used yet  
+    if len(customer_data) == 0:
+        # creating customer
+        customer = stripe.Customer.create(
+        email=email, payment_method=payment_method_id)
+    
+    else:
+        customer = customer_data[0]
+        extra_msg = "Customer already existed."
+    
+    stripe.PaymentIntent.create(
+    customer=customer, 
+    payment_method=payment_method_id,  
+    currency='pln', # you can provide any currency you want
+    amount=1500, # I modified amount to distinguish payments
+    confirm=True)
+    
+    return Response(status=status.HTTP_200_OK,
+        data = {
+            'message': 'Success',
+            'data': {'customer_id': customer.id,
+            'extra_msg': extra_msg},
+        })
+
+###########################################################################################################################
+#RazorPay Start
+###########################################################################################################################
+
+from .models import Order
+from .serailizers import OrderSerializer
+import json
+
+import environ
+import razorpay
+
+env= environ.Env()
+
+# you have to create .env file in same folder where you are using environ.Env()
+# reading .env file which located in api folder
+environ.Env.read_env()
+
+@api_view(['POST'])
+def start_payment(request):
+    #request.data is coming from frontend
+    amount = request.data['amount']
+    name = request.data['name']
+
+    # setup razorpay client this is the client to whome user is paying money that's you
+    client = razorpay.Client(auth=(env('PUBLIC_KEY'), env('SECRET_KEY')))
+
+    # create razorpay order
+    # the amount will come in 'paise' that means if we pass 50 amount will become
+    # 0.5 rupees that means 50 paise so we have to convert it in rupees. So, we will 
+    # mumtiply it by 100 so it will be 50 rupees.
+    payment = client.order.create({"amount": int(amount) * 100, 
+                                   "currency": "INR", 
+                                   "payment_capture": "1"})
+    # we are saving an order with isPaid=False because we've just initialized the order
+    # we haven't received the money we will handle the payment succes in next 
+    # function
+    order = Order.objects.create(order_product=name, 
+                                 order_amount=amount, 
+                                 order_payment_id=payment['id'])
+    
+    serializer = OrderSerializer(order)
+
+    data = {
+        "payment": payment,
+        "order": serializer.data
+    }
+
+    return Response(data)
+
+
+def handle_payment_success(request):
+    # request.data is coming from the frontend
+
+    res = json.loads(request.data["response"])
+
+    """res will be:
+    {'razorpay_payment_id': 'pay_G3NivgSZLx7I9e', 
+    'razorpay_order_id': 'order_G3NhfSWWh5UfjQ', 
+    'razorpay_signature': '76b2accbefde6cd2392b5fbf098ebcbd4cb4ef8b78d62aa5cce553b2014993c0'}
+    this will come from frontend which we will use to validate and confirm the payment
+    """
+
+    ord_id = ""
+    raz_pay_id = ""
+    raz_signature = ""
+    # res.keys() will give us list of keys in res
+    for key in res.keys():
+        if key == 'razorpay_order_id':
+            ord_id = res[key]
+        elif key == 'razorpay_payment_id':
+            raz_pay_id = res[key]
+        elif key == 'razorpay_signature':
+            raz_signature = res[key]
+
+    # get order by payment_id which we've created earlier with isPaid=False
+    order = Order.objects.get(order_payment_id=ord_id)
+
+    # we will pass this whole data in razorpay client to verify the payment
+    data = {
+        'razorpay_order_id': ord_id,
+        'razorpay_payment_id': raz_pay_id,
+        'razorpay_signature': raz_signature
+    }
+
+    client = razorpay.Client(auth=(env('PUBLIC_KEY'), env('SECRET_KEY')))
+
+    # razorpay client if it is "valid" then check will return None
+    check = client.utility.verify_payment_signature(data)
+
+    if check is not None:
+        print("Redirect to error url or error page")
+        return Response({'error': 'Something went wrong'})
+
+    # if payment is successful that means check is None then we will turn isPaid=True
+    order.isPaid = True
+    order.save()
+
+    res_data = {
+        'message': 'payment successfully received!'
+    }
+
+    return Response(res_data)
+
+
+
+
+
