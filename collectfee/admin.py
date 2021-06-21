7888c9d323ea272308b89d7f9fc617163fec1dd8
Nikhilesh <nikhilesh.debbarma062@gmail.com>
2021-06-21 18:58:18 +0530

    hahaha

--- collectfee/admin.py
+++ collectfee/admin.py
@@ -2,13 +2,11 @@ from django.contrib import admin
 from .models import Order
 # Register your models here.
 
-
+@admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    date_hierarchy = 'order_date'
-    list_display = ('order_product', 'order_amount', 'order_payment_id', 'isPaid', 'order_date')
+    list_display = ['order_product', 'order_amount', 'order_payment_id', 'isPaid', 'order_date']
 
 
-admin.site.register(Order)
 
 
 
--- payment_gate/urls.py
+++ payment_gate/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('payment/', include('collectfee.urls')),
+    path('', include('collectfee.urls')),
 
 ]
