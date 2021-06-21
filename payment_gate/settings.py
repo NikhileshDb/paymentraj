f385f19032a9aef748e35c5d0662b82722d0cd47
Nikhilesh <nikhilesh.debbarma062@gmail.com>
2021-06-21 15:13:18 +0530

    jhyd

--- payment_gate/settings.py
+++ payment_gate/settings.py
@@ -156,5 +156,10 @@ CORS_ORIGIN_ALLOW_ALL = True
 
 
 import os
+STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join('staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
