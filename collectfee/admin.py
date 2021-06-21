ef119ddf170e88b4599eb3e480b3593f912555c8
Nikhilesh <nikhilesh.debbarma062@gmail.com>
2021-06-21 18:35:01 +0530

    loko

--- collectfee/admin.py
+++ collectfee/admin.py
@@ -1,5 +1,11 @@
-from django.contrib import admin
+from django.contrib import admin, 
 from .models import Order
 # Register your models here.
 
-admin.site.register(Order)
+
+class OrderAdmin(admin.ModelAdmin):
+    date_hierarchy = 'order_date'
+    list_display = ('order_product', 'order_amount', 'order_payment_id', 'isPaid', 'order_date')
+
+
+
